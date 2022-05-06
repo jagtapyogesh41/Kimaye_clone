1d18d174733291e6f038ea96ae7ea9683885f0ec
Yogesh Jagtap <jagtapyogesh41@gmail.com>
2022-05-06 17:36:51 +0530

    adding grow page

---
+++ Grow/Grow.js
@@ -0,0 +1,291 @@
+var BlogPostArr=[
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/articles/AdobeStock_334275008-2-opt2-1900px_540x.jpg?v=1627119119",
+        title:"Top Fruit Safety Tips You Should Be Swearing By Every Time You Consume Them .",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"NUTRITION",
+        Date:"24",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://post.healthline.com/wp-content/uploads/2020/07/fruit-platter-thumb_1-2-732x549.jpg",
+        title:"Fruits Rich In Vitamin C That You Should Be Eating Everyday",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"child",
+        Date:"19 ",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"http://cdn.shopify.com/s/files/1/0449/5225/6667/articles/AdobeStock_77286816-min__1625567830_114.143.85.97_1200x1200.jpg?v=1625638895",
+        title:"How To Remove And Clean Harmful Pesticides From FruitsTop ",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"storage",
+        Date:"07 ",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"http://cdn.shopify.com/s/files/1/0449/5225/6667/articles/Blog_-_Benefits_of_fruits_1200x1200.jpg?v=1625147388",
+        title:"Why Should You Eat Fruits More Often? ",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"child",
+        Date:"07 ",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/Untitled_design_51-min.png?v=1614342058",
+        title:"Grapes: How they reached India, types and health benefits",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"storage",
+        Date:"01 ",
+        month:"JAN",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/immune_system_1728x-min.jpg?v=1614342138",
+        title:"Make Immunity your Best Friend",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"NUTRITION",
+        Date:"07 ",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/pomegranate.jpg?v=1605992883",
+        title:"Nourish yourself with these 'fruitful' nutrients",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"child",
+        Date:"07 ",
+        month:"FEB",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/healthy_heart.jpg?v=1605992624",
+        title:"Healthy at Heart",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"NUTRITION",
+        Date:"27 ",
+        month:"DEC",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/b8.jpg",
+        title:"Craving for Something Delicious During the Lockdown? We Got You!",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"child",
+        Date:"07 ",
+        month:"NOV",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/pomegranate-1.jpg?v=1605992943",
+        title:"A fruitful monsoon diet",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"lifestyle",
+        Date:"03 ",
+        month:"NOV",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/consume_fruits.jpg?v=1606108168",
+        title:"Do fruits have a time table",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"storage",
+        Date:"07 ",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    {
+        avatar:"https://cdn.shopify.com/s/files/1/0449/5225/6667/files/working_out_at_home.jpg?v=1605991783",
+        title:"Meet your health goals during the lockdown",
+        desc:"Fresh fruits bring with them a horde of health benefits, but it’s the way we consume them that is more important. While fruits have the ability to prevent many diseases such as cancer, heart stroke,...1",
+        Categori:"lifestyle",
+        Date:"07 ",
+        month:"JUL",
+        year:"2022",
+        ReadMore:"Read More",
+        imgmail:"https://mpng.subpng.com/20180718/twz/kisspng-computer-icons-stock-photography-email-clip-art-simblo-5b4faed2d78991.2976490215319487548829.jpg",
+    },
+    
+    
+    
+    
+    
+];
+
+DisplayData(BlogPostArr)
+
+
+// document.getElementById('nutrition').onclick = function changeContent() {
+//     if 
+
+//     alert("yes")
+ 
+//  }
+
+// sort by nutrition
+function SortByNutrition(){
+    // window.location.href="nut.html";
+    var checknut = document.querySelector("#nutri").value
+
+    var filternut = BlogPostArr.filter(function(element){
+        
+        return (element.Categori == "NUTRITION");
+        // window.location.href="nut.html";
+    })
+    DisplayData(filternut)
+    
+    
+}
+// sort by SortByLifestyle
+function SortByLifestyle(){
+    var checknut = document.querySelector("#lifestyle").value
+
+    var filternut = BlogPostArr.filter(function(element){
+        
+        return (element.Categori == "lifestyle")
+    })
+    DisplayData(filternut)
+    
+}
+
+function SortByChild(){
+    var checknut = document.querySelector("#child").value
+
+    var filternut = BlogPostArr.filter(function(element){
+        
+        return (element.Categori == "child")
+    })
+    DisplayData(filternut)
+    
+}
+
+function SortByStorage(){
+    var checknut = document.querySelector("#storage").value
+
+    var filternut = BlogPostArr.filter(function(element){
+        
+        return (element.Categori == "storage")
+    })
+    DisplayData(filternut)
+    
+}
+
+
+
+
+
+// main contain
+
+function DisplayData(data){
+    // event.preventDefault();
+
+    document.querySelector('.div1').innerHTML="";
+
+    
+
+data.forEach(function(ele){
+
+    
+    const box = document.createElement('div');
+    box.setAttribute("id","Posts1")
+
+    const add1 = document.createElement('a');
+    add1.setAttribute("href","blogpost.html")
+
+    const Avatar = document.createElement('img');
+    Avatar.setAttribute("src",ele.avatar)
+    // console.log(Avatar);
+    add1.append(Avatar)
+
+    const Title = document.createElement('h3');
+    Title.setAttribute("id","h3cg")
+    Title.innerText=ele.title;
+    // console.log(Title);
+
+    const contant = document.createElement('p');
+    contant.setAttribute("class","p123")
+    contant.innerText=ele.desc;
+    // console.log(contant);
+
+    const More = document.createElement('a');
+    More.setAttribute("id","readMore")
+    More.setAttribute("href","blogpost.html")
+    More.innerText="Read More";
+    // More.onclick="blogpost.html"
+    // More.innerHTML=
+    // console.log(More);
+    const divimg = document.createElement('div');
+    divimg.setAttribute("class","divimg")
+
+    const image = document.createElement('img');
+    image.setAttribute("class","imgmail")
+    image.setAttribute("src",ele.imgmail)
+
+    const addimg = document.createElement('a');
+    addimg.setAttribute("id","addtoimg")
+    addimg.setAttribute("href","https://kimaye.com/pages/contact-us")
+    // console.log(image);
+
+    addimg.append(image)
+    divimg.append(addimg)
+
+
+    const y_dvi = document.createElement('div');
+    y_dvi.setAttribute("class","date_year")
+
+    const date = document.createElement('h5');
+    date.innerText=ele.Date;
+
+    const Month = document.createElement('h6');
+    Month.innerText=ele.month;
+
+    const Year = document.createElement('h5');
+    Year.innerText=ele.year;
+
+        const line = document.createElement('div');
+        line.setAttribute("class","hrline")
+    const line1 = document.createElement('hr');
+    const line2 = document.createElement('hr');
+    line.append(line1,line2)
+
+    y_dvi.append(date,Month)
+
+    box.append(add1,Title,contant,More,divimg,line,y_dvi);
+
+    document.querySelector('.div1').append(box);
+});
+
+}
+
+
+
+
+
+
+
