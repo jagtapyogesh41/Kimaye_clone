c194bd4ad0ec5cfce4d12e9c1e0037d8bab3245d
srinu8008 <104018702+srinu8008@users.noreply.github.com>
2022-05-09 12:48:17 +0530

    Create AllFruits.js

---
+++ shopPage/script/AllFruits.js
@@ -0,0 +1,223 @@
+var fruitsObj =[ 
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/image_540x.jpg?v=1620889338",
+        name:"Kimaye Jamun (250 gm)",
+        cost:"₹189",
+
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/MicrosoftTeams-image_1_540x.jpg?v=1649415923",
+        name:"Kimaye Summer Combo",
+        cost:"₹477"
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover_19473cce-96ae-4149-aaa1-508f17ac6c28_540x.jpg?v=1648186636",
+        name:"Kimaye Watermelon (1 pc)",
+        cost:"₹79",
+
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover_540x.jpg?v=1647932788",
+        name:"Kimaye Green Apples (Pack of 3 pc & 6 pc)",
+        cost:"₹109",
+
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/MangoCover_908c34e6-fcdb-4564-918e-48d7baeaf628_540x.jpg?v=1645445684",
+        name:"Kimaye Alphonso Mangoes",
+        cost:"₹276 - ₹1499",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/5e8b2ee6-ce8e-42e9-959f-d43c544f586a_540x.png?v=1616326949",
+        name:"Kimaye Blueberries (125g)",
+        cost:"₹419",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Chiku-cover_7b7702b6-dbf6-4d51-aabd-70df2f5e4195_540x.jpg?v=1646830414",
+        name:"Kimaye Sapota (6 pcs)",
+        cost:"₹76",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover-Mango-badami_900x.jpg?v=1620473077",
+        name:"kimaye Badami Mangoes  ( 3 pcs)",
+        cost:"₹169",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Avocado_900x.jpg?v=1622045722",
+        name:"Kimaye Avocado Imported (1pc ≈ 250g)",
+        cost:"₹315",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/463b5516-614a-4a66-9556-f4a09e45afbc_360x.png?v=1614326370",
+        name:"Kimaye Pear Green (Pack of 2 & 4)",
+        cost:"₹110 - ₹209"
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/a0cfceb5-76a4-4994-9d37-978c6dc3b309_360x.png?v=1611811512",
+        name:"Kimaye Imported Oranges (Pack of 3 & 6)",
+        cost:"₹109 - ₹431",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/royal-gala-apple-cover_360x.jpg?v=1643012816",
+        name:"Kimaye Royal Gala Apples (Pack of 3 & 6)",
+        cost:"₹163 - ₹315",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/069bdaa3-eca4-4f4c-8f02-970a626f7be3_360x.png?v=1616393545",
+        name:"Kimaye Grapes (Flame Seedless (500g & 2kg).",
+        cost:"₹109 - ₹431",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/9ebb0dfa-3897-42a3-97be-5a0ecb8491f8_540x.png?v=1615016898",
+        name:"Kimaye Gapes (Super Sonaka Seedless(500g).",
+        cost:"₹99",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/dbe751de-009c-4454-a3f1-42375be8bb15_540x.png?v=1614859367",
+        name:"Kimaye Three Grape Combo",
+        cost:"₹152",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Grapescombo_1_360x.png?v=1642492575",
+        name:"Kimaye Blueberry-Green Grapes Combo",
+        cost:"₹492",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/GPS-BB_360x.png?v=1642492910",
+        name:"Kimaye Grapefruit (Pack of 2 & 4 pcs)",
+        cost:"₹150 - ₹289",    
+    },
+    {    img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/dragon-fruit--white-cover_360x.jpg?v=1632470660",
+        name:"Kimaye Dragon Fruit (1 pc)",
+        cost:"₹89",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover-sun-melon_360x.jpg?v=1620314805",
+        name:"Kimaye Sun Melon (1 pc)",
+        cost:"₹63",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/washington-applecover_540x.jpg?v=1641449752",
+        name:"Kimaye Washington Apples (pack of 2 pc & 4 pc)",
+        cost:"₹165 - ₹311",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover-Mango_5fc755ae-5f58-4127-b1f9-30bceaa56e3d_360x.jpg?v=1620127827",
+        name :"Kimaye Kesar Mangoes (Pack of 3 & 6)",
+        cost:"₹405 - ₹799",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/golden-kiwi-cover_360x.jpg?v=1625147312",
+        name:"Kimaye Golden Kiwis (3 pcs)  ",
+        cost:"₹199",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/bd07b8e7-3643-4a98-996e-c85fd7d133a8_360x.png?v=1614765886",
+        name:"Red Globe Imported (400g)",
+        cost:"₹219",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/CopyofGuava_Main_Clean_360x.png?v=1606828311",
+        name:"Kimaye Thai Guava (1 pc~ 0.4-0.5k)",
+        cost:"₹69",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/1_3311b3de-1a38-4175-8e8c-ac9577ec85df-min_360x.jpg?v=1614324760",
+        name:"Kimaye Bananas Robusta (5-6 PCS)",
+        cost:"₹42",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/221f2788-1a08-45ae-bdaf-7432d7ea0156_360x.png?v=1614326590",
+        name:"Kimaye Pomegranates (Pack of 2 & 4)",
+        cost:"₹150 - ₹289",
+    },
+   
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/1_6875ada8-f1e6-4589-b1fd-6f13a86b8ddb-min_360x.jpg?v=1614326164",
+        name:"Kimaye Kiwis (3 pcs~ 300g)",
+        cost:"₹133",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/62cef687-de59-4f5a-acf3-4df1238c3e29-min_360x.png?v=1614324446",
+        name:"Kimaye Arils-Coco Cup (125g)",
+        cost:"55-140",
+    },
+    {
+        img_url:"https://cdn.shopify.com/s/files/1/0449/5225/6667/products/CopyofMosambi_Main_FixedShadows-min_360x.png?v=1614327057",
+        name:"Kimaye Mosambi (Pack of 3 & 6)",
+        cost:"77-145",
+    },
+
+];
+document.getElementById("bhogi")
+
+fruitsObj.map(function(elem){
+    var box = document.createElement('div');
+
+    const button = document.createElement('button');
+    button.innerText = 'Add to Cart';
+    button.setAttribute('id','cart')
+
+    var img = document.createElement('img');
+    img.setAttribute('id','img')
+    img.src=elem.img_url;
+    img.addEventListener('mouseover',()=>{
+        button.style.display='block';
+    })
+
+    img.addEventListener('mouseout',()=>{
+        button.style.display='none';
+    })
+    
+    // img.append(button);
+
+    var name = document.createElement('p');
+    name.innerText=elem.name
+
+    var cost = document.createElement('p');
+    cost.innerText=elem.cost
+
+    box.append(img,button,name,cost)
+
+    bhogi.append(box)
+});
+
+
+
+v
+
+document.querySelector("#cart").addEventListener("click", function () {
+    addToCart(item)
+
+    function addToCart(item) {
+        if (cartarr.includes(item)) {
+            alert("already add to chart")
+        }
+        else {
+            alert("Added");
+            window.location.href="cart.html"
+        }
+        cartarr.push(item)
+        localStorage.setItem("datas", JSON.stringify(cartarr))
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
